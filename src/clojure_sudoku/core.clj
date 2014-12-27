16c8b93fb379168052b4411063e52b4ad46afee1
Tobias Heide <theide@stereokrauts.com>
2014-12-27 19:03:09 +0100

    Function to apply a function to a subfield of a field

--- src/clojure_sudoku/core.clj
+++ src/clojure_sudoku/core.clj
@@ -17,9 +17,24 @@
   [field]
   (apply map vector field))
 
-(defn map-row-recurse
-  "Returns a new field with applied fn to the row row-num"
-  [fn row-num current-row field]
+(defn field-visitor-col-recurse
+  [fn predicate row-num field-row]
+  (loop [current-col 0
+         result []
+         field-row-rest field-row]
+    (let [is-last-col (= current-col (count field-row))
+          current-cell-contents (atom (first field-row-rest))]
+      (if is-last-col
+        result
+        (do (if (predicate current-col row-num)
+              (swap! current-cell-contents #(fn %)))
+          (recur (inc current-col)
+                 (conj result @current-cell-contents)
+                 (rest field-row-rest)))))))
+
+(defn field-visitor-recurse
+  "Returns a new field with applied fn to all fields matching the predicate"
+  [fn predicate current-col current-row field]
   (loop [current-row current-row
          result []
          field-rest field]
@@ -27,19 +42,47 @@
           current-row-contents (atom (first field-rest))]
       (if is-last-row
         result
-        (do 
-          (if (= row-num current-row)
-            (swap! current-row-contents #(map fn %)))
-          (recur (inc current-row) (conj result @current-row-contents) (rest field-rest)))))))
+        (do
+          (swap! current-row-contents #(field-visitor-col-recurse fn predicate current-row %))
+          (recur (inc current-row)
+                 (conj result @current-row-contents)
+                 (rest field-rest)))))))
+
+(defn field-visitor
+  "Returns a new field with applied fn to all fields matching the predicate"
+  [fn predicate field]
+  (field-visitor-recurse fn predicate 0 0 field))
+
+
+(defn rownum-equals?
+  [match-row x y]
+  (= match-row y))
+
+(defn colnum-equals?
+  [match-col x y]
+  (= match-col x))
+
+(defn subfield-equals?
+  [subfield-definition x y]
+  (let [top-left     (first subfield-definition)
+        bottom-right (nth subfield-definition 1)]
+    (and  (>= x (first top-left))
+          (<  x (first bottom-right))
+          (>= y (nth top-left 1))
+          (<  y (nth bottom-right 1))
+          )))
 
 (defn map-row
   "Applies fn to sudoku row row-num in field"
   [fn row-num field]
-  (map-row-recurse fn row-num 0 field))
+  (field-visitor fn (partial rownum-equals? row-num) field))
 
 (defn map-col
   "Applies fn to sudoku column col-num in field"
   [fn col-num field]
-  (transpose
-         (map-row fn col-num (transpose field))))
+  (field-visitor fn (partial colnum-equals? col-num) field))
 
+(defn map-subfield
+  "Applies fn to a subfield of field"
+  [fn sub-field field]
+  (field-visitor fn (partial subfield-equals? sub-field) field))
--- test/clojure_sudoku/core_test.clj
+++ test/clojure_sudoku/core_test.clj
@@ -27,16 +27,35 @@
   (testing "A complete field that contains empty fields can be parsed"
     (is (= [[1 nil] [nil 4]] (parse-field ["1." ".4"])))))
 
+(deftest subfield-equals?-test
+  (testing "Subfield is correctly calcualted"
+    (is (= true (subfield-equals? [[0 0] [2 2]] 1 1)))))
+
 (deftest map-row-test
   (testing "A map-function that only applies to the first row"
     (is (= [[2 3] 
             [3 4]] 
-           (map-row #(+ 1 %) 0 [[1 2]
-                                [3 4]])))))
+           (map-row #(inc %) 
+                    0
+                    [[1 2]
+                     [3 4]])))))
 
 (deftest map-col-test
   (testing "A map-function that only applies to the second column"
     (is (= [[1 3] 
             [3 5]] 
-           (map-col #(+ 1 %) 1 [[1 2]
-                                [3 4]])))))
+           (map-col #(inc %) 
+                    1
+                    [[1 2]
+                     [3 4]])))))
+
+(deftest map-subfield-test
+  (testing "A map-function that only applies to a sub-field of the complete field"
+    (is (= [[2 3 3] 
+            [5 6 6]
+            [7 8 9]] 
+           (map-subfield #(inc %)
+                         [[0 0] [2 2]]
+                        [[1 2 3]
+                         [4 5 6]
+                         [7 8 9]])))))
